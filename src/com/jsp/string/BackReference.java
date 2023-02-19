9f01605868293afefa05e0bd68a22de9ef85c17b
Madhura <Choukanavarmadhura6@gmail.com>
2023-02-19 17:08:19 +0530

    Regex

---
+++ src/com/jsp/string/BackReference.java
@@ -0,0 +1,18 @@
+package com.jsp.string;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BackReference {
+
+	public static void main(String[] args) {
+		String s= "a11b22ccd";
+		Pattern p=Pattern.compile("([a-z])([0-9])\\2");
+		Matcher m=p.matcher(s);
+		while(m.find())
+		{
+			System.out.println(m.group());	
+			
+		}
+}
+}
---
+++ src/com/jsp/string/RegexCheck.java
@@ -0,0 +1,11 @@
+package com.jsp.string;
+
+public class RegexCheck {
+
+	public static void main(String[] args) {
+		String s="select * from employees where name=\"madhura\";";
+		System.out.println(s);
+		
+		}
+
+}
